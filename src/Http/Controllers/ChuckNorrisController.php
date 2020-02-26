6bcbbd57b59e6e46972cbbfed0e175b0ed71b24b
camilamilagros <camila.chavez@pucp.pe>
2020-02-25 20:08:31 -0500

    Apply fixes from StyleCI (#6)

--- src/Http/Controllers/ChuckNorrisController.php
+++ src/Http/Controllers/ChuckNorrisController.php
@@ -3,14 +3,13 @@
 namespace Camilamilagros\ChuckNorrisJokes\Http\Controllers;
 
 use Camilamilagros\ChuckNorrisJokes\Facades\ChuckNorris;
-use Illuminate\Routing\Controller;
 
 class ChuckNorrisController
 {
     public function __invoke()
     {
         return view('chuck-norris::joke', [
-            'joke' => ChuckNorris::getRandomJoke()
+            'joke' => ChuckNorris::getRandomJoke(),
         ]);
     }
 }
